CypherOS/packages_apps_Settings

Settings: Statusbar clock customizations [1/2]
Change-Id: I370cf9f9dc1d118c2a31cc5c8b08843dad382925

src/com/android/settings/DateTimeSettings.java
@@ -24,6 +24,7 @@ import android.os.UserManager;
 import android.provider.SearchIndexableResource;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.aoscp.datetime.AdvancedDateTimePreferenceController;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.dashboard.SummaryLoader;
 import com.android.settings.datetime.AutoTimePreferenceController;
@@ -95,6 +96,7 @@ public class DateTimeSettings extends DashboardFragment implements
                 activity, this /* UpdateTimeAndDateCallback */, autoTimePreferenceController));
         controllers.add(new DatePreferenceController(
                 activity, this /* UpdateTimeAndDateCallback */, autoTimePreferenceController));
+		controllers.add(new AdvancedDateTimePreferenceController(activity));
         return controllers;
     }
 

src/com/android/settings/aoscp/datetime/AdvancedDateTimePreferenceController.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2017 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.aoscp.datetime;
+
+import android.content.Context;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+
+public class AdvancedDateTimePreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin {
+			
+    private static final String KEY_ADVANCED_DATE_TIME = "advanced_date_time";
+
+    public AdvancedDateTimePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_ADVANCED_DATE_TIME;
+    }
+}

src/com/android/settings/aoscp/datetime/ClockSecondsPreferenceController.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.aoscp.datetime;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class ClockSecondsPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_STATUS_BAR_CLOCK_SECONDS = "status_bar_clock_seconds";
+
+    public ClockSecondsPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_STATUS_BAR_CLOCK_SECONDS;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        int value = Settings.System.getInt(
+                mContext.getContentResolver(), Settings.System.STATUS_BAR_CLOCK_SECONDS, 0);
+        ((SwitchPreference) preference).setChecked(value != 0);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value = (Boolean) newValue;
+        Settings.System.putInt(
+                mContext.getContentResolver(), Settings.System.STATUS_BAR_CLOCK_SECONDS, value ? 1 : 0);
+        return true;
+    }
+}

src/com/android/settings/aoscp/datetime/ClockStylePreferenceController.java
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.aoscp.datetime;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.util.Log;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class ClockStylePreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "ClockStylePref";
+	private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock_style";
+  
+    private ListPreference mClockStyle;
+
+    public ClockStylePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return STATUS_BAR_CLOCK_STYLE;
+    }
+
+	@Override
+    public void updateState(Preference preference) {
+        final ListPreference mClockStyle = (ListPreference) preference;
+        final Resources res = mContext.getResources();
+        if (mClockStyle != null) {
+            int clockStyleKey = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.STATUSBAR_CLOCK_STYLE, 0,
+                    UserHandle.USER_CURRENT);
+            String styleKey = String.valueOf(clockStyleKey);
+            mClockStyle.setValue(styleKey);
+            updateClockStyleSummary(mClockStyle, styleKey);
+        }
+    }
+
+	@Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        try {
+            String styleKey = (String) newValue;
+            Settings.System.putIntForUser(mContext.getContentResolver(), Settings.System.STATUSBAR_CLOCK_STYLE,
+                    Integer.parseInt(styleKey), UserHandle.USER_CURRENT);
+            updateClockStyleSummary((ListPreference) preference, styleKey);
+        } catch (NumberFormatException e) {
+            Log.e(TAG, "Could not set clock style", e);
+        }
+        return true;
+    }
+	
+	private void updateClockStyleSummary(Preference mClockStyle, String styleKey) {
+        if (styleKey != null) {
+            String[] values = mContext.getResources().getStringArray(R.array
+                    .clock_style_values);
+            final int summaryArrayResId = R.array.clock_style_entries;
+            String[] summaries = mContext.getResources().getStringArray(summaryArrayResId);
+            for (int i = 0; i < values.length; i++) {
+                if (styleKey.equals(values[i])) {
+                    if (i < summaries.length) {
+                        mClockStyle.setSummary(summaries[i]);
+                        return;
+                    }
+                }
+            }
+        }
+
+        mClockStyle.setSummary("");
+        Log.e(TAG, "Invalid clock style value: " + styleKey);
+    }
+}

src/com/android/settings/aoscp/datetime/DateDisplayPreferenceController.java
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.aoscp.datetime;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.util.Log;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class DateDisplayPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "DateDisplayPref";
+	private static final String CLOCK_DATE_DISPLAY = "clock_date_display";
+  
+    private ListPreference mDateDisplay;
+
+    public DateDisplayPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return CLOCK_DATE_DISPLAY;
+    }
+
+	@Override
+    public void updateState(Preference preference) {
+        final ListPreference mDateDisplay = (ListPreference) preference;
+        final Resources res = mContext.getResources();
+        if (mDateDisplay != null) {
+            int dateDisplayKey = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.STATUSBAR_CLOCK_DATE_DISPLAY, 0,
+                    UserHandle.USER_CURRENT);
+            String dateKey = String.valueOf(dateDisplayKey);
+            mDateDisplay.setValue(dateKey);
+            updateDateDisplaySummary(mDateDisplay, dateKey);
+        }
+    }
+
+	@Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        try {
+            String dateKey = (String) newValue;
+            Settings.System.putIntForUser(mContext.getContentResolver(), Settings.System.STATUSBAR_CLOCK_DATE_DISPLAY,
+                    Integer.parseInt(dateKey), UserHandle.USER_CURRENT);
+            updateDateDisplaySummary((ListPreference) preference, dateKey);
+        } catch (NumberFormatException e) {
+            Log.e(TAG, "Could not set date display", e);
+        }
+        return true;
+    }
+	
+	private void updateDateDisplaySummary(Preference mDateDisplay, String dateKey) {
+        if (dateKey != null) {
+            String[] values = mContext.getResources().getStringArray(R.array
+                    .clock_date_display_values);
+            final int summaryArrayResId = R.array.clock_date_display_entries;
+            String[] summaries = mContext.getResources().getStringArray(summaryArrayResId);
+            for (int i = 0; i < values.length; i++) {
+                if (dateKey.equals(values[i])) {
+                    if (i < summaries.length) {
+                        mDateDisplay.setSummary(summaries[i]);
+                        return;
+                    }
+                }
+            }
+        }
+
+        mDateDisplay.setSummary("");
+        Log.e(TAG, "Invalid date display value: " + dateKey);
+    }
+}

src/com/android/settings/aoscp/datetime/ShowClockPreferenceController.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2018 CypherOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.aoscp.datetime;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class ShowClockPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_STATUS_BAR_SHOW_CLOCK = "status_bar_show_clock";
+
+    public ShowClockPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_STATUS_BAR_SHOW_CLOCK;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        int value = Settings.System.getInt(
+                mContext.getContentResolver(), Settings.System.STATUS_BAR_CLOCK, 1);
+        ((SwitchPreference) preference).setChecked(value != 0);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value = (Boolean) newValue;
+        Settings.System.putInt(
+                mContext.getContentResolver(), Settings.System.STATUS_BAR_CLOCK, value ? 1 : 0);
+        return true;
+    }
+}

brjshshrma/android_packages_apps_Settings

Signed-off-by: brjshshrma <brajesh.sharma87@gmail.com>

src/com/android/settings/DateTimeSettings.java
@@ -24,6 +24,7 @@ import android.os.UserManager;
 import android.provider.SearchIndexableResource;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.custom.datetime.AdvancedDateTimePreferenceController;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.dashboard.SummaryLoader;
 import com.android.settings.datetime.AutoTimePreferenceController;
@@ -95,6 +96,7 @@ public class DateTimeSettings extends DashboardFragment implements
                 activity, this /* UpdateTimeAndDateCallback */, autoTimePreferenceController));
         controllers.add(new DatePreferenceController(
                 activity, this /* UpdateTimeAndDateCallback */, autoTimePreferenceController));
+        controllers.add(new AdvancedDateTimePreferenceController(activity));
         return controllers;
     }
 

src/com/android/settings/custom/datetime/AdvancedDateTimePreferenceController.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.custom.datetime;
+
+import android.content.Context;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+
+public class AdvancedDateTimePreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin {
+			
+    private static final String KEY_ADVANCED_DATE_TIME = "advanced_date_time";
+
+    public AdvancedDateTimePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_ADVANCED_DATE_TIME;
+    }
+}

src/com/android/settings/custom/datetime/ClockSecondsPreferenceController.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2018
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.custom.datetime;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class ClockSecondsPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_STATUS_BAR_CLOCK_SECONDS = "status_bar_clock_seconds";
+
+    public ClockSecondsPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_STATUS_BAR_CLOCK_SECONDS;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        int value = Settings.System.getInt(
+                mContext.getContentResolver(), Settings.System.STATUS_BAR_CLOCK_SECONDS, 0);
+        ((SwitchPreference) preference).setChecked(value != 0);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value = (Boolean) newValue;
+        Settings.System.putInt(
+                mContext.getContentResolver(), Settings.System.STATUS_BAR_CLOCK_SECONDS, value ? 1 : 0);
+        return true;
+    }
+}

src/com/android/settings/custom/datetime/ClockStylePreferenceController.java
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2018
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.custom.datetime;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.util.Log;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class ClockStylePreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "ClockStylePref";
+    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock_style";
+  
+    private ListPreference mClockStyle;
+
+    public ClockStylePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return STATUS_BAR_CLOCK_STYLE;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        final ListPreference mClockStyle = (ListPreference) preference;
+        final Resources res = mContext.getResources();
+        if (mClockStyle != null) {
+            int clockStyleKey = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.STATUSBAR_CLOCK_STYLE, 0,
+                    UserHandle.USER_CURRENT);
+            String styleKey = String.valueOf(clockStyleKey);
+            mClockStyle.setValue(styleKey);
+            updateClockStyleSummary(mClockStyle, styleKey);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        try {
+            String styleKey = (String) newValue;
+            Settings.System.putIntForUser(mContext.getContentResolver(), Settings.System.STATUSBAR_CLOCK_STYLE,
+                    Integer.parseInt(styleKey), UserHandle.USER_CURRENT);
+            updateClockStyleSummary((ListPreference) preference, styleKey);
+        } catch (NumberFormatException e) {
+            Log.e(TAG, "Could not set clock style", e);
+        }
+        return true;
+    }
+
+    private void updateClockStyleSummary(Preference mClockStyle, String styleKey) {
+        if (styleKey != null) {
+            String[] values = mContext.getResources().getStringArray(R.array
+                    .clock_style_values);
+            final int summaryArrayResId = R.array.clock_style_entries;
+            String[] summaries = mContext.getResources().getStringArray(summaryArrayResId);
+            for (int i = 0; i < values.length; i++) {
+                if (styleKey.equals(values[i])) {
+                    if (i < summaries.length) {
+                        mClockStyle.setSummary(summaries[i]);
+                        return;
+                    }
+                }
+            }
+        }
+
+        mClockStyle.setSummary("");
+        Log.e(TAG, "Invalid clock style value: " + styleKey);
+    }
+}

src/com/android/settings/custom/datetime/DateDisplayPreferenceController.java
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2018
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.custom.datetime;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.util.Log;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class DateDisplayPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "DateDisplayPref";
+    private static final String CLOCK_DATE_DISPLAY = "clock_date_display";
+  
+    private ListPreference mDateDisplay;
+
+    public DateDisplayPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return CLOCK_DATE_DISPLAY;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        final ListPreference mDateDisplay = (ListPreference) preference;
+        final Resources res = mContext.getResources();
+        if (mDateDisplay != null) {
+            int dateDisplayKey = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.STATUSBAR_CLOCK_DATE_DISPLAY, 0,
+                    UserHandle.USER_CURRENT);
+            String dateKey = String.valueOf(dateDisplayKey);
+            mDateDisplay.setValue(dateKey);
+            updateDateDisplaySummary(mDateDisplay, dateKey);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        try {
+            String dateKey = (String) newValue;
+            Settings.System.putIntForUser(mContext.getContentResolver(), Settings.System.STATUSBAR_CLOCK_DATE_DISPLAY,
+                    Integer.parseInt(dateKey), UserHandle.USER_CURRENT);
+            updateDateDisplaySummary((ListPreference) preference, dateKey);
+        } catch (NumberFormatException e) {
+            Log.e(TAG, "Could not set date display", e);
+        }
+        return true;
+    }
+
+    private void updateDateDisplaySummary(Preference mDateDisplay, String dateKey) {
+        if (dateKey != null) {
+            String[] values = mContext.getResources().getStringArray(R.array
+                    .clock_date_display_values);
+            final int summaryArrayResId = R.array.clock_date_display_entries;
+            String[] summaries = mContext.getResources().getStringArray(summaryArrayResId);
+            for (int i = 0; i < values.length; i++) {
+                if (dateKey.equals(values[i])) {
+                    if (i < summaries.length) {
+                        mDateDisplay.setSummary(summaries[i]);
+                        return;
+                    }
+                }
+            }
+        }
+
+        mDateDisplay.setSummary("");
+        Log.e(TAG, "Invalid date display value: " + dateKey);
+    }
+}

src/com/android/settings/custom/datetime/ShowClockPreferenceController.java
@@ -1,0 +1,57 @@
+/*
+ * Copyright (C) 2018
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.custom.datetime;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class ShowClockPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_STATUS_BAR_SHOW_CLOCK = "status_bar_show_clock";
+
+    public ShowClockPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_STATUS_BAR_SHOW_CLOCK;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        int value = Settings.System.getInt(
+                mContext.getContentResolver(), Settings.System.STATUS_BAR_CLOCK, 1);
+        ((SwitchPreference) preference).setChecked(value != 0);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value = (Boolean) newValue;
+        Settings.System.putInt(
+                mContext.getContentResolver(), Settings.System.STATUS_BAR_CLOCK, value ? 1 : 0);
+        return true;
+    }
+}